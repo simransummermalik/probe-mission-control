aafb6330adf9d75088e3025218d417942b4d5aee
Simran Malik <smalik12@charlotte.edu>
2025-10-23 13:21:17 -0400

    springboot initialization

---
+++ backend/missioncontrol/missioncontrol/src/main/java/com/example/missioncontrol/MissionControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.missioncontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissionControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissionControlApplication.class, args);
+	}
+
+}
